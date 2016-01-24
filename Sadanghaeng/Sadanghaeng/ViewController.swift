1da3a691a065c42aad0d915271514ca0bfd85708
Donghyun Go <godong9@gmail.com>
2016-01-24 16:08:38 +0900

    Init project

---
+++ Sadanghaeng/Sadanghaeng/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sadanghaeng
+//
+//  Created by GoDonghyun on 2016. 1. 24..
+//  Copyright © 2016년 Sadanghaeng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
