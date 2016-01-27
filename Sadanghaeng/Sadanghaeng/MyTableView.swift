ecc51394aa569842293aa7ca2246bdb9f2db672a
garin <gairn0112@naver.com>
2016-01-27 17:27:03 +0900

    Appicon,TableView update
    추가추가

---
+++ Sadanghaeng/Sadanghaeng/MyTableView.swift
@@ -0,0 +1,37 @@
+//
+//  MyTableView.swift
+//  Sadanghaeng
+//
+//  Created by garin on 2016. 1. 27..
+//  Copyright © 2016년 GARIN. All rights reserved.
+//
+
+import UIKit
+
+class MyTableView: UITableViewController {
+    
+    var MyArray : Array<String> = ["하나","둘","셋"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return MyArray.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as! UITableViewCell
+        
+        cell.textLabel?.text = MyArray[indexPath.row]
+        return cell
+    }
+}
