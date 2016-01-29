b2e5587204d0a317d0fe3d4c3bed319fca087022
garin <gairn0112@naver.com>
2016-01-29 10:39:07 +0900

    update

--- Sadanghaeng/Sadanghaeng/WriteViewController.swift
+++ Sadanghaeng/Sadanghaeng/WriteViewController.swift
@@ -21,4 +21,16 @@ class WriteViewController: UIViewController {
         print("Text: \(text)")
     }
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // 다른 곳 탭하면 키보드 숨기기
+        let tap : UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "dismissKeyboard")
+        view.addGestureRecognizer(tap)
+    }
+    
+    // 키보드 숨기는 함수
+    func dismissKeyboard() {
+        view.endEditing(true)
+    }
 }
