3f88b207ebdbe3fbbfb0ea941cdc2d482ecda00e
garin <gairn0112@naver.com>
2016-01-29 16:58:36 +0900

    email 인증 수정

---
+++ Sadanghaeng/Sadanghaeng/CustomView.swift
@@ -0,0 +1,17 @@
+//
+//  CustomView.swift
+//  Sadanghaeng
+//
+//  Created by garin on 2016. 1. 29..
+//  Copyright © 2016년 Sadanghaeng. All rights reserved.
+//
+
+import UIKit
+
+class CustomView: UIView {
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.layer.cornerRadius = 10
+    }
+
+}
---
+++ Sadanghaeng/Sadanghaeng/VerifyViewController.swift
@@ -0,0 +1,13 @@
+//
+//  VerifyViewController.swift
+//  Sadanghaeng
+//
+//  Created by garin on 2016. 1. 29..
+//  Copyright © 2016년 Sadanghaeng. All rights reserved.
+//
+
+import UIKit
+
+class VerifyViewController: UIViewController {
+
+}
